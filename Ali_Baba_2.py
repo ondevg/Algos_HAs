5f8c473db4b19f945a70b5d5cffe4e82a5ca722f
ondevg <evg.ondar@gmail.com>
2021-01-09 19:00:07 +0300

    AliBaba_2 improved

--- Ali_Baba_2.py
+++ Ali_Baba_2.py
@@ -1,11 +1,21 @@
 import random
 
 
-def
+def less(i1, i2):
+    return i1[0] * i2[1] < i2[0] * i1[1]
+
+
+def equal(i1, i2):
+    return i1[0] * i2[1] == i2[0] * i1[1]
+
+
+def more(i1, i2):
+    return i1[0] * i2[1] > i2[0] * i1[1]
+
 
 def partition(x, l, r, pivot):
-    il = 1
-    ir = 1
+    il = l
+    ir = l
     for i in range(l, r):
         if x[i] < pivot and ir < r:
             x[il], x[i] = x[i], x[il]
@@ -28,4 +38,21 @@ def qsort(x, l=0, r=None):
         qsort(x, l, il)
         qsort(x, ir, r)
 
-N, w = map()
+
+N, w = list(map(int, input().split()))
+x = []
+for i in range(N):
+    x.append(tuple(map(int, input().split())))
+qsort(x)
+x = x[::-1]
+
+s = 0
+i = 0
+while (i < N) and (w >= x[i][1]):
+    s += x[i][0]
+    w -= x[i][1]
+    i += 1
+if i < N:
+    s += (x[i][0] * w // x[i][1])
+
+print(s)
